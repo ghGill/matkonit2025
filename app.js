1304ee5acd76a3f638a9f076eca9d6219dadced2
Gil <gil190267@gmail.com>
2025-09-28 17:37:23 +0300

    add fallback when refresh page to run the app

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 const express = require("express");
-// const config = require('config');  // use default.json in dev mode and production.json in build mode.
 const dotenv = require('dotenv').config();  // add all variables defined in .env file to process.env (usage: process.env.VAR_NAME)
 const { envVar } = require('./services/env');
 const categoryRouter = require("./routes/category");
 const recipeRouter = require("./routes/recipe");
 const dbRouter = require("./routes/db");
-// const settingsRouter = require("./routes/settings");
-// const userRouter = require("./routes/user");
-// const filesRouter = require("./routes/files");
 const { initDB } = require('./services/db');
-// const authMiddleware = require('./middleware/authToken')
 const cors = require('cors');
 const path = require('path');
 
@@ -19,11 +14,6 @@ app.use(express.json())
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public', 'app')));
-
-// if (envVar('STORAGE_TYPE') === 'DISK') {
-//     const moviesFolderName = envVar('MOVIES_FOLDER');
-//     app.use(`/${moviesFolderName}`, express.static(`${envVar('DISK_ROOT_PATH')}/${moviesFolderName}`));
-// }
 
 app.use(cors({
     origin: function (origin, callback) {
@@ -41,47 +31,14 @@ app.use(cors({
 })); // Enable CORS for all origins
 
 // *********** ROUTES *************************
-// app.use('/settings', authMiddleware.verifyAuthToken, settingsRouter);
-// app.use('/auth', authRouter);
 app.use('/db', dbRouter);
 app.use('/category', categoryRouter);
 app.use('/recipe', recipeRouter);
 
-// app.use('/files', filesRouter);
-
-// app.get('/config', (req, res) => {
-//     try {
-//         let appEnvVariables = {};
-
-//         Object.keys(process.env).forEach(e => {
-//             if (e.toUpperCase().startsWith('VITE_'))
-//                 appEnvVariables[e] = process.env[e];
-//         })
-
-//         res.status(200).json({ success: true, data: appEnvVariables });
-//     }
-//     catch (e) {
-//         res.status(500).json({ success: false, message: e.message, data: {} })
-//     }
-// })
-
 // fallback for all routes
-// app.use((req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'app', 'index.html'));
-// });
-
-// ==========================================================
-// const storage = require('./services/storage');
-
-// const s3 = new STORAGE_S3();
-// async function test() {
-// console.log(await storage.createFolder({folderPath:'C:/_RT-ED/projects/news-app/backend/public/movies/one/aaa/test'}));
-// console.log(await storage.getFolderContent({folderPath:'C:/_RT-ED/projects/news-app/backend/public/movies'}));
-// console.log(await storage.deleteFile({filePath:'C:/_RT-ED/projects/news-app/backend/public/movies/test.mp4'}));
-// }
-// test();
-// return;
-// ==========================================================
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'app', 'index.html'));
+});
 
 initDB()
     .then(result => {
@@ -98,8 +55,3 @@ initDB()
     .catch(e => {
         console.log(e);
     })
-
-// const appPort = envVar("APP_PORT");
-// const server = app.listen(appPort, async () => {
-//     console.log(`Server is listening on port ${appPort}`);
-// })
